706085e8c922abb50f167733c2c3dabe72ac45b6
xhguleixin123 <18017158729@163.com>
2021-12-28 14:55:45 +0800

    ✨ feat: 窗体初始化

--- fast_mass_spring/main.cpp
+++ fast_mass_spring/main.cpp
@@ -3,8 +3,76 @@
  * @Description: 
  */
 #include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+// settings
+static uint32_t SCR_WIDTH = 800;
+static uint32_t SCR_HEIGHT = 600;
+
+static GLFWwindow* window;
+
+// Init Window
+static int InitGlfwWindow();
+static void Terminate();
+
+// callback function
+
 
 int main()
 {
-    std::cout << "test" << std::endl;
+    if(InitGlfwWindow())
+    {
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    Terminate();
+    return 0;
+}
+
+
+
+static int InitGlfwWindow()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+
+static void Terminate()
+{
+    glfwTerminate();
 }
